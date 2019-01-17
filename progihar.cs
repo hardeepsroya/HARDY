50e063b21e05a2cfbc719a33e504b148be988506
hardeepsroya <46795362+hardeepsroya@users.noreply.github.com>
2019-01-17 17:02:27 -0500

    Add files via upload

---
+++ progihar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c0703625
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dogs = new List<string>();
+            dogs.Add("bichon");
+            dogs.Add("poodle");
+            dogs.Add("retriver");
+             //foreach (var dog in dogs)
+            // {
+           //    Console.WriteLine(dog + " ");
+          //  }
+
+            Console.WriteLine(dogs[1]);
+            int[] a1 = new int[] { 1, 3, 5, 7, 9, 11 };
+            foreach (int i in a1)
+            {
+                Console.WriteLine("the value of i is {0} ")
+            }
+        }
+    }
+}
